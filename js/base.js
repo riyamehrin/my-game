32ebbbab5f6720635ac06bc1320ee367abd96418
riyamehrin <86054695+riyamehrin@users.noreply.github.com>
2022-02-19 12:59:30 +0530

    Add files via upload

--- js/base.js
+++ js/base.js
@@ -18,20 +18,40 @@ class Base {
        this.conrolPlayer();
        this.createHill();
        this.createCoin();
-       drawSpirtes()
+       this.scaleCoin();
+       drawSprites() 
    }
    createHill(){
-       if(frameCount%100==0){
-           var hill=createSprite(random(width/2+100,width/2-100),0,20,20);
+       if(frameCount%200==0){
+           var hill=createSprite(random(width/2+75,width/2-75),height/2-75,20,20);
            hill.velocityY=5;
+           hill.velocityX=random(-2,2)
+           var r=Math.floor(random(0,2))
+           switch(r){
+               case 0:hill.addImage(obs1);
+               break;
+               case 1:hill.addImage(obs2);
+               break;
+               case 2:hill.addImage(obs3);
+               break;
+           }
            this.hillGroup.push(hill)
        }
    }
    createCoin(){
-    if(frameCount%50==0){
-        var coin=createSprite(random(width/2+100,width/2-100),0,20,20);
+    if(frameCount%100==0){
+        var coin=createSprite(random(width/2+75,width/2-75),height/2-75,20,20);
         coin.velocityY=5;
+        coin.velocityX=random(-3,3)
+        coin.addImage(coinimg);
+        coin.scale=0.03;
+        //coin.scale+=0.01
         this.coinGroup.push(coin)
+    }
+}
+scaleCoin(){
+    for(var i=0;i<this.coinGroup.length;i++){
+        this.coinGroup[i].scale+=0.001;
     }
 }
 }
--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
 var bg,bg2,form,system,code,security;
 var score=0;
-
+var coinimg;
+var obs1,obs2,obs3
 function preload() {
   bg = loadImage("day.gif");
-  bg2 = loadImage("night.gif")
+  bg2 = loadImage("night.gif");
+  coinimg = loadImage("goldCoin.png");
+  obs1= loadImage("obiyas1.png");
+  obs2= loadImage("obiyas2.png");
+  obs3= loadImage("obiyas3.png");
 }
 
 function setup() {

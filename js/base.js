2607423389f9598de279a38c3ad00fb68457f1dc
riyamehrin <86054695+riyamehrin@users.noreply.github.com>
2022-02-21 19:00:21 +0530

    Add files via upload

--- js/base.js
+++ js/base.js
@@ -1,17 +1,20 @@
 class Base {
     constructor(){
-        this.player=createSprite(width/2,height-100,50,50)
+        this.player=createSprite(width/2,height-150,50,50)
         this.hillGroup=[];
         this.coinGroup=[];
-
-
+        this.player.addImage(bike)
+        this.player.scale=1
+        this.player.rotation=0
     }
     conrolPlayer(){
         if(keyDown('LEFT_ARROW')){
             this.player.x-=12
+            this.player.rotation=10
         }
         if(keyDown('RIGHT_ARROW')){
             this.player.x+=12
+            this.player.rotation=-10
         }
     }
    show(){
@@ -19,20 +22,25 @@ class Base {
        this.createHill();
        this.createCoin();
        this.scaleCoin();
+       this.scaleHill();
        drawSprites() 
    }
    createHill(){
        if(frameCount%200==0){
-           var hill=createSprite(random(width/2+75,width/2-75),height/2-75,20,20);
+           var hill=createSprite(random(width/2+75,width/2-75),height/2+50,20,20);
            hill.velocityY=5;
            hill.velocityX=random(-2,2)
-           var r=Math.floor(random(0,2))
+           var r=Math.floor(random(0,3))
+          // var r=2;
            switch(r){
                case 0:hill.addImage(obs1);
+               hill.scale=0.4;
                break;
                case 1:hill.addImage(obs2);
+               hill.scale=0.2;
                break;
                case 2:hill.addImage(obs3);
+               hill.scale=0.1;
                break;
            }
            this.hillGroup.push(hill)
@@ -52,6 +60,11 @@ class Base {
 scaleCoin(){
     for(var i=0;i<this.coinGroup.length;i++){
         this.coinGroup[i].scale+=0.001;
+    }
+}
+scaleHill(){
+    for(var i=0;i<this.hillGroup.length;i++){
+        this.hillGroup[i].scale+=0.007;
     }
 }
 }
--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var bg,bg2,form,system,code,security;
 var score=0;
 var coinimg;
-var obs1,obs2,obs3
+var obs1,obs2,obs3,bike
 function preload() {
   bg = loadImage("day.gif");
   bg2 = loadImage("night.gif");
@@ -9,15 +9,23 @@ function preload() {
   obs1= loadImage("obiyas1.png");
   obs2= loadImage("obiyas2.png");
   obs3= loadImage("obiyas3.png");
+  bike=loadImage("bike.png");
 }
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
+  //createCanvas(300,500);
   player = new Base()
   
 }
 
 function draw() {
-  background(bg);
+  background(0);
+  image(bg,0,0,width,height)
   player.show()
+}
+function keyReleased(){
+  if(keyCode==37 || keyCode==38){
+    player.player.rotation=0
+  }
 }
